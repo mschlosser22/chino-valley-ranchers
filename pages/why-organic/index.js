90feab36e6f88221e7cae7ef6cde7f36aad40726
jschlosser11 <jake_schlosser11@yahoo.com>
2021-05-25 10:37:40 -0400

    why organic page

---
+++ pages/why-organic/index.js
@@ -0,0 +1,85 @@
+import Head from 'next/head'
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { useForm, usePlugin, useCMS } from 'tinacms'
+import { InlineForm, InlineBlocks } from 'react-tinacms-inline'
+import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
+
+import { Nav } from '../../components/Nav'
+import { Footer } from '../../components/footer/Footer'
+import { heroBlock } from '../../components/hero/Hero'
+import { whyOrganicBlock } from '../../components/why-organic/WhyOrganic'
+
+export default function Products({ file, isPreview}) {
+
+  const cms = useCMS()
+
+  const formConfig = {
+    id: '../../content/why-organic/index.json',
+    initialValues: file,
+    label: 'Why Organic',
+    fields: [
+      
+    ],
+    onSubmit() {
+      cms.alerts.success('Saved!')
+    },
+  }
+
+  // Registers a JSON Tina Form
+  //const [data, form] = useGithubJsonForm(file, formConfig)
+  const [, form] = useGithubJsonForm(file, formConfig)
+  usePlugin(form)
+
+  useGithubToolbarPlugins()
+
+  return (
+    <>
+    <div className={`relative`}>
+      <Head>
+        <title>Chino Valley Ranchers | Why Organic</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <InlineForm form={form}>
+        <InlineBlocks name="blocks" blocks={PAGE_BLOCKS} />
+      </InlineForm>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+const PAGE_BLOCKS = {
+  hero: heroBlock,
+  whyOrganic: whyOrganicBlock
+}
+
+export const getStaticProps = async function({
+  preview,
+  previewData,
+}) {
+
+  if (preview) {
+    return getGithubPreviewProps({
+    ...previewData,
+    fileRelativePath: 'content/why-organic/index.json',
+    parse: parseJson,
+    isPreview: true
+    })
+  }
+
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/why-organic/index.json',
+        data: (await import('../../content/why-organic/index.json')).default,
+      }
+    },
+  }
+
+}
