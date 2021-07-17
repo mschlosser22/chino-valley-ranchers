5298f711266f84d2dc54d8a67365febd962316ff
Mike Schlosser <mike.schlosser@pearson.com>
2021-07-16 23:36:10 -0400

    update

--- components/tinacms/EditLink.tsx
+++ components/tinacms/EditLink.tsx
@@ -3,10 +3,25 @@ export function EditLink(props) {
     const {cms} = props
 
     return(
-        <div className="fixed bottom-0 w-full bg-grey-300 z-50">
-            <button onClick={() => cms.toggle()}>
-                {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
-            </button>
+        <div className="py-20 mt-48">
+            <div className="max-w-7xl mx-auto px-4 sm:px-8 lg:px-12">
+                <div className="p-2 rounded-lg bg-indigo-600 shadow-lg sm:p-3 text-white">
+                    <div className="flex items-center justify-between flex-wrap">
+                        <div className="w-0 flex-1 flex items-center">
+                            <p className="ml-3 font-medium text-white truncate">
+                                <span>
+                                    {cms.enabled ? 'Click the button to logout' : 'Click the button to Login and edit the site'}
+                                </span>
+                            </p>
+                        </div>
+                        <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                            <button onClick={() => cms.toggle()} className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
+                                {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
