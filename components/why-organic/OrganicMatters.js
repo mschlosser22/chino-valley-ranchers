9f3d08614210b3aee57fd92a10133c32c9f7516a
Mike Schlosser <mike.schlosser@pearson.com>
2021-07-19 16:49:57 -0400

    update

--- components/why-organic/OrganicMatters.js
+++ components/why-organic/OrganicMatters.js
@@ -2,7 +2,7 @@ import { propTypes } from 'react-markdown'
 import { InlineTextarea, InlineImage, BlocksControls } from 'react-tinacms-inline'
 
 
-export function OrganicMatters() {
+export function OrganicMatters(props) {
     return (
         <div>
             <div className="relative -mt-12 pt-36 bg-no-repeat bg-cover" style={{ backgroundImage: `url('/images/bg-gray-stripes-alt.png')` }}>
@@ -18,13 +18,30 @@ export function OrganicMatters() {
                         </div>
                         <div className="lg:col-span-4 col-span-12 lg:-mt-12 mx-auto">
                             <img src={props.imageRight.src} className="w-64 lg:w-full" alt={props.imageRight.alt} />
-                            <img src={props.imageRightAlt} className="lg:-ml-16 lg:-mt-2 -mt-0 -ml-0 w-52 lg:w-full" alt={props.imageRightAlt.alt} />
+                            <img src={props.imageRightAlt.src} className="lg:-ml-16 lg:-mt-2 -mt-0 -ml-0 w-52 lg:w-full" alt={props.imageRightAlt.alt} />
                         </div>
                     </div>
                     <div className="lg:mt-16 mt-12 lg:pb-12 pb-6 ">
-                        <h3 className="text-chinored lg:text-4xl text-3xl lg:mx-0 mx-8 uppercase font-ultra ">What Organic Means For Your Health</h3>
+                        <h3 className="text-chinored lg:text-4xl text-3xl lg:mx-0 mx-8 uppercase font-ultra ">
+                            <InlineTextarea name="listHeading" />
+                        </h3>
                     </div>
                     <div className="grid grid-cols-12 gap-6">
+                        {props.list.map((item, index) =>
+                            <div key={index}>
+                                <div className="lg:col-span-4 col-span-12">
+                                    <h4 className="text-2xl uppercase black font-ultra lg:mx-0 mx-8">
+                                        <InlineTextarea name="title" />
+                                    </h4>
+                                </div>
+                                <div className="lg:col-span-8 col-span-12">
+                                    <p className="font-lato lg:text-2xl tracking-wide lg:mx-0 mx-8 lg:pr-16 pr-0">
+                                        <InlineTextarea name="description" />
+                                    </p>
+                                </div>
+                                <hr className="solid border-chinodarkgray col-span-12"></hr>
+                            </div>
+                        )}
                         <div className="lg:col-span-4 col-span-12">
                             <h4 className="text-2xl uppercase black font-ultra lg:mx-0 mx-8">Preservative<br className="lg:block hidden" /> Free</h4>
                         </div>
@@ -72,6 +89,53 @@ export const organicMattersBlock = {
 
         },
         fields: [
-
+            {
+                name: "heading",
+                label: 'Heading',
+                component: "textarea"
+            },
+            {
+                name: "description",
+                label: 'Description',
+                component: "textarea"
+            },
+            {
+                name: "imageRight",
+                label: 'Image Right',
+                component: "group",
+                fields: [
+                    {
+                        name: 'src',
+                        label: 'src',
+                        component: 'image',
+                        parse: media => `/images/${media.filename}`,
+                        uploadDir: () => '/images'
+                    },
+                    {
+                        name: 'alt',
+                        label: 'Alt',
+                        component: 'text'
+                    }
+                ]
+            },
+            {
+                name: "imageRightAlt",
+                label: 'Image Right Bottom',
+                component: "group",
+                fields: [
+                    {
+                        name: 'src',
+                        label: 'src',
+                        component: 'image',
+                        parse: media => `/images/${media.filename}`,
+                        uploadDir: () => '/images'
+                    },
+                    {
+                        name: 'alt',
+                        label: 'Alt',
+                        component: 'text'
+                    }
+                ]
+            }
         ],
     }}
