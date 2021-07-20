27a7dffef37815038dd2c455f4e66a3e1c444aee
Mike Schlosser <mike.schlosser@pearson.com>
2021-07-20 00:23:09 -0400

    update

--- components/being-humane/HenParadise.js
+++ components/being-humane/HenParadise.js
@@ -37,10 +37,14 @@ export function HenParadise(props) {
 
                 {/* Page Heading */}
                 <div className="lg:text-center z-50 pt-12 lg:py-16 max-w-4xl mx-auto px-8 lg:px-0">
-                    <h1 className="text-3xl lg:text-6xl text-chinored font-ultra uppercase lg:mb-12 mb-6">Hello Hen Paradise!</h1>
-                    <p className="font-lato lg:text-xl tracking-wide leading-normal pb-12 font-lato font-bold">At Chino Valley Ranchers we strive to provide an environment that allows our chickens to live and act in their natural ways. They have unlimited access to sunlight, premium grain feed, and plenty of fresh air and water. In fact, their fresh water is drawn from our own wells.</p>
-                    <p className="font-lato lg:text-xl tracking-wide leading-normal pb-12 font-lato font-bold">Our cage free hen houses are specially built to give the birds freedom to roam the floor, and also are equipped with special roosting and nesting areas. All of our hen houses are environmentally controlled to maintain comfortable temperatures.</p>
-                    <p className="font-lato lg:text-xl tracking-wide leading-normal font-bold">Besides enjoying the interior amenities of the cage free houses, our free range and organic chickens are free to go outside—for sunning, socializing or dust bathing—anytime they want.</p>
+                    <h1 className="text-3xl lg:text-6xl text-chinored font-ultra uppercase lg:mb-12 mb-6">
+                        <InlineTextarea name="heading" />
+                    </h1>
+                    {props.list.map((item, index) =>
+                        <p className="font-lato lg:text-xl tracking-wide leading-normal pb-12 font-lato font-bold">
+                            {item.description}
+                        </p>
+                    )}
                 </div>
 
                 <div className="flex relative" id="chickens">
@@ -149,7 +153,23 @@ export const henParadiseBlock = {
         label: 'Hen Paradise Component',
         defaultItems: [],
         fields: [
-
+            {
+                name: "heading",
+                label: "Heading",
+                component: "text"
+            },
+            {
+                name: "list",
+                label: "Paragraphs",
+                component: "group-list",
+                fields: [
+                    {
+                        name: "description",
+                        label: "Paragraph",
+                        component: "textarea"
+                    }
+                ]
+            }
         ]
     }
 }
