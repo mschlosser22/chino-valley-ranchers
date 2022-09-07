ec3cf3c8085d1f0b78b4cdce2f650f99ebffaea2
Mike Schlosser <mike.schlosser@pearson.com>
2022-09-07 19:13:02 -0400

    add gif and resize the banner/texts/images

--- components/button/CtaButton.js
+++ components/button/CtaButton.js
@@ -4,7 +4,7 @@ export function CtaButton(props) {
 
   return(
 
-  <a href={button.link.url} target="_blank" className={`xl:text-5xl px-5 py-2  bg-chinored text-white uppercase font-din font-bold xs:text-sm text-xs md:text-xl lg:text-4xl rounded-lg tracking-wide cursor-pointer z-60`}>{button.text}</a>
+  <a href={button.link.url} target="_blank" className={`2xl:text-5xl xl:text-2xl lg:text-xl md:text-md px-5 py-2  bg-chinored text-white uppercase font-din font-bold xs:text-sm text-xs md:text-xl lg:text-4xl rounded-lg tracking-wide cursor-pointer z-60`}>{button.text}</a>
   )
 
 }
