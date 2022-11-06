cfc6ce85175b48dd49e7a427e4a6f80163e79ca9
Mike Schlosser <mike.schlosser@pearson.com>
2022-11-06 08:19:49 -0500

    new banner

--- components/button/CtaButton.js
+++ components/button/CtaButton.js
@@ -4,7 +4,7 @@ export function CtaButton(props) {
 
   return(
 
-  <a href={button.link.url} target="_blank" className={`2xl:text-5xl xl:text-2xl lg:text-xl md:text-md px-5 py-2  bg-chinored text-white uppercase font-din font-bold xs:text-sm text-xs md:text-xl lg:text-4xl rounded-lg tracking-wide cursor-pointer z-60`}>{button.text}</a>
+  <a href={button.link.url} target="_blank" className={`block 3xl:text-5xl 2xl:text-3xl xl:text-2xl lg:text-xl md:text-lg sm:text-md text-sm 2xl:px-12 xl:px-10 lg:px-6 md:px-6 sm:px-4 px-2 sm:py-2 py-1 mr-2 text-center bg-chinored text-white uppercase font-din font-bold xs:text-sm text-xs md:text-xl lg:text-4xl rounded-lg cursor-pointer`}>{button.text}</a>
   )
 
 }
