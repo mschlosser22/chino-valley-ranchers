8a1f9275248a0211dfaf3e5079ed337b8b88c124
Mike Schlosser <ms122r@gmail.com>
2023-10-05 14:02:20 +0200

    update gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,11 +35,11 @@ export default class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{
             __html: `
             <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5BWNNM7F');</script>
+            })(window,document,'script','dataLayer','GTM-5BWNNM7F');
             <!-- End Google Tag Manager -->
 
             `
@@ -50,10 +50,8 @@ export default class MyDocument extends Document {
         <body>
           {/* Google Tag Manager - add the <body> part here */}
           <noscript dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BWNNM7F"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BWNNM7F"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
           }}></noscript>
           <Main />
           <NextScript />
