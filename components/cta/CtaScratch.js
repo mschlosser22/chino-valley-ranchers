b820cb78ddb6ff964b93a49e995e057f77d1e836
mschlosser22 <ms122r@gmail.com>
2025-02-18 18:51:24 -0500

    update banner

--- components/cta/CtaScratch.js
+++ components/cta/CtaScratch.js
@@ -6,7 +6,7 @@ import styles from "@tailwindcss/typography/src/styles";
 const buttonProps = {
   button: {
     link: {
-      url: "https://youtu.be/rZaYeqYh-Ik",
+      url: "https://youtu.be/bGx9D4xiqdU",
     },
     text: "Watch the video",
   },
@@ -49,6 +49,7 @@ export function CtaScratch(props) {
         ctaHeight ? "max-h-screen lg:max-h-max" : "max-h-0"
       }`}
     >
+      <a href="https://youtu.be/bGx9D4xiqdU" target="_blank" rel="noopener noreferrer">
       <div
         className={`bg-scratchBanner bg-cover bg-center bg-no-repeat pt-4 text-white z-60 xl:h-full lg:h-64 sm:h-48 xs:h-32 h-24 top-0 transform transition duration-700 ease-in-out origin-top ${
           isLoaded ? "scale-y-100" : "scale-y-0"
@@ -57,7 +58,7 @@ export function CtaScratch(props) {
         <div className="grid grid-cols-2 3xl:gap-80 2xl:gap-64 xl:gap-56 lg:gap-56 md:gap-36 sm:gap-28 gap-16">
           <div className="col-span-1 p-0 m-0">
             <img
-              src="https://res.cloudinary.com/dmfgntgym/image/fetch/f_auto/q_auto:eco/https://www.chinovalleyranchers.com//images/suitcase.png"
+              src="https://res.cloudinary.com/dmfgntgym/image/fetch/f_auto/q_auto:eco/https://www.chinovalleyranchers.com/images/pocket.jpg"
               className="3xl:h-96 xl:h-80 lg:h-56 md:h-44 sm:h-44  h-20 3xl:pl-40 xl:pl-32 lg:pl-12 md:pl-20 sm:pl-12 pl-6"
             />
             {/* <Image
@@ -70,16 +71,7 @@ export function CtaScratch(props) {
 
           <div className="col-span-1">
             <div className="sm:flex block relative">
-              <img
-                src="https://res.cloudinary.com/dmfgntgym/image/fetch/f_auto/q_auto:eco/https://www.chinovalleyranchers.com//images/scratch.png"
-                className="3xl:h-80 xl:h-64 lg:h-48 md:h-44 sm:h-36 sm:h-32 xs:h-28 h-16 md:ml-0 ml-4 z-10"
-              />
-              {/* <Image
-                            src='/images/womanBanner.png'
-                            height={396}
-                            width={428}
-                            alt="Chino Valley Ranchers"
-                        /> */}
+
               <div>
                 <div className="3xl:-ml-8 2xl:-ml-7 xl:-ml-6 lg:-ml-5 md:-ml-4 sm:-ml-3 -ml-16 absolute bottom-0 3xl:mb-20 2xl:mb-14 xl:mb-10 lg:mb-10 md:mb-8 sm:mb-6 -mb-2">
                   <CtaButton {...buttonProps} />
@@ -98,6 +90,7 @@ export function CtaScratch(props) {
           X
         </button>
       </div>
+      </a>
     </div>
   );
 }
